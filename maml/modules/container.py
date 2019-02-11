fb2712d661a0689cba06fe5f2019138f21d9cf0f
Tristan Deleu <tristan.deleu@gmail.com>
2019-02-11 10:37:25 -0500

    Fix ordering in MetaSequential

--- maml/modules/container.py
+++ maml/modules/container.py
@@ -7,10 +7,10 @@ from maml.modules.utils import get_subdict
 class MetaSequential(nn.Sequential, MetaModule):
     def forward(self, input, params=None):
         for name, module in self._modules.items():
-            if isinstance(module, nn.Module):
-                input = module(input)
-            elif isinstance(module, MetaModule):
+            if isinstance(module, MetaModule):
                 input = module(input, params=get_subdict(params, name))
+            elif isinstance(module, nn.Module):
+                input = module(input)
             else:
                 raise TypeError('The module must be either a torch module '
                     '(inheriting from `nn.Module`), or a `MetaModule`. '

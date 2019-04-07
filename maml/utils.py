ddd3bdb431a9fc43c097b93830b9cf3f176903ce
Tristan Deleu <tristan.deleu@gmail.com>
2019-04-07 08:43:07 -0400

    Fix tensors_to_device

--- maml/utils.py
+++ maml/utils.py
@@ -22,10 +22,10 @@ def tensors_to_device(tensors, device=torch.device('cpu')):
     if isinstance(tensors, torch.Tensor):
         return tensors.to(device=device)
     elif isinstance(tensors, (list, tuple)):
-        return type(tensors)(tensors_to_device(tensor)
+        return type(tensors)(tensors_to_device(tensor, device=device)
             for tensor in tensors)
     elif isinstance(tensors, (dict, OrderedDict)):
-        return type(tensors)([(name, tensors_to_device(tensor))
+        return type(tensors)([(name, tensors_to_device(tensor, device=device))
             for (name, tensor) in tensors.items()])
     else:
         raise NotImplementedError()

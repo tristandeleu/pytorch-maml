6b8336c386308c91ae02c0c86d8656df879f9d11
Tristan Deleu <tristan.deleu@gmail.com>
2019-10-09 15:40:28 -0700

    Add __all__ in metalearners

--- maml/metalearners/__init__.py
+++ maml/metalearners/__init__.py
@@ -1,2 +1,4 @@
 from maml.metalearners.maml import ModelAgnosticMetaLearning, MAML, FOMAML
 from maml.metalearners.meta_sgd import MetaSGD
+
+__all__ = ['ModelAgnosticMetaLearning', 'MAML', 'FOMAML', 'MetaSGD']
